0a0d10d63b03d429c1d2f05b1dc6ca431ec26838
Michal Dudziak <dudziak.michal@gmail.com>
2025-08-15 10:49:26 +0200

    basic file for calling openai

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,25 @@
-from dotenv import load_dotenv
-import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+information = """
+Elon Reeve Musk FRS (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman, known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
+Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He received bachelor's degrees from the University of Pennsylvania in 1997 before moving to California, United States, to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
+"""
+
+
 if __name__ == '__main__':
-    load_dotenv()
     print('Hello LangChain\n')
-    print(os.environ['COOL_API_KEY'])
+
+    summary_template ="""
+    given the information about a person from I want you to create: 
+    1. a short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(input_variable="information", template=summary_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="o4-mini")
+    chain = summary_prompt_template | llm
+
+    res = chain.invoke(input={"information": information})
+
